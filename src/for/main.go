ca57f66b6d80450b5fd8e91e9298df5ad04feb74
g5niusx <g5niusx@gmail.com>
2018-10-14 23:18:21 +0800

    :construction: Work in progress.

--- src/for/main.go
+++ src/for/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 打印矩形
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 5; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+	// 打印一半矩形
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
+	// 打印金字塔
+	for i := 1; i <= 3; i++ {
+		for k := 1; k <= 3-i; k++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= 2*i-1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println("")
+	}
 }
