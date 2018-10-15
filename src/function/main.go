1e43eac0f6d51972dd58791fc6b12d58dad05116
g5niusx <g5niusx@gmail.com>
2018-10-15 22:49:22 +0800

    :construction: Work in progress.

--- src/function/main.go
+++ src/function/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// go中的基本数据类型和数组在函数中都是值传递，不会改变原来的值,在java中数组是引用传递会修改原来的值
@@ -25,7 +28,15 @@ func main() {
 	test := func(a int, b int) int {
 		return a * b
 	}(8, 9)
-	fmt.Printf("匿名函数返回值 %v \t", test)
+	fmt.Printf("匿名函数返回值 %v \n", test)
+	// 测试闭包
+	u := addUpper()
+	fmt.Printf("闭包函数的值为: %v \n", u(1))
+	fmt.Printf("闭包函数的值为: %v \n", u(1))
+	// 闭包实践
+	s := makeSuffix(".jpg")
+	fmt.Printf("测试结果:%v \n", s("test"))
+	fmt.Printf("测试结果:%v \n", s("自拍.jpg"))
 }
 
 // 测试值传递
@@ -58,6 +69,31 @@ func swap(a *int, b *int) {
 	*b = temp
 }
 
+// 闭包
+func addUpper() upper {
+	// n申明在了函数中，对于返回的函数来说。n是不会改变的
+	n := 10
+	return func(i int) int {
+		n += i
+		return n
+	}
+}
+
+// 传入一个后缀，如果传入的闭包参数带有后缀则直接返回，否则拼接后缀
+func makeSuffix(suffix string) suffix {
+	return func(s string) string {
+		if strings.HasSuffix(s, suffix) {
+			return s
+		}
+		s += suffix
+		return s
+	}
+}
+
 type mySum func(int8, int8) int8
 
 type sumSub func(int, int) (int, int)
+
+type upper func(int) int
+
+type suffix func(string) string
