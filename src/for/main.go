e5f028729b3565da6fddffee938f8ddfc85d571e
g5niusx <g5niusx@gmail.com>
2018-10-15 17:18:25 +0800

    :construction: Work in progress.

--- src/for/main.go
+++ src/for/main.go
@@ -27,4 +27,42 @@ func main() {
 		}
 		fmt.Println("")
 	}
+
+	test(4)
+	// 斐波那契数 0,1,1,2,3,5
+	fmt.Println("斐波那契数:", test2(3))
+	fmt.Println(test3(3))
+	fmt.Println("桃子的总数:", feed(9))
+}
+
+func test(num int8) {
+	if num > 2 {
+		num--
+		test(num)
+	}
+	fmt.Println(num)
+}
+
+// 斐波那契数
+func test2(num int16) int16 {
+	if num == 1 || num == 2 {
+		return 1
+	} else {
+		return test2(num-1) + test2(num-2)
+	}
+}
+
+func test3(num int8) int8 {
+	if num == 1 {
+		return 3
+	}
+	return 2*test3(num-1) + 1
+}
+
+// 🐒吃桃子问题,f(x) = (f(x+1)+1)*2
+func feed(day int8) int8 {
+	if day == 10 {
+		return 1
+	}
+	return (feed(day+1) + 1) * 2
 }
---
+++ src/function/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// go中的基本数据类型和数组在函数中都是值传递，不会改变原来的值,在java中数组是引用传递会修改原来的值
+	var day int8 = 8
+	test(day)
+	fmt.Println(day)
+	// 对函数进行重命名
+	var funcSum mySum = sum
+	fmt.Printf("funcSum的类型 %T \n", funcSum)
+	fmt.Printf("funcSum的运算结果 %v \n", funcSum(1, 3))
+}
+
+func test(day int8) {
+	day--
+}
+
+func sum(a int8, b int8) int8 {
+	return a + b
+}
+
+type mySum func(int8, int8) int8
