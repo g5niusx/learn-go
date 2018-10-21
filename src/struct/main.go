076fd00c10bba480f438b21ce76ed1d4dea4f411
g5niusx <g5niusx@gmail.com>
2018-10-21 20:36:54 +0800

    :construction: Work in progress.

--- src/struct/main.go
+++ src/struct/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // 结构体是值传递
 func main() {
-	var newCat *Cat = new(Cat)
+	var newCat = new(Cat)
 	newCat.Age = 2
 	fmt.Printf("使用new方式创建的对象:%v,修改年龄为:%v\n", newCat, newCat.Age)
 	cat := Cat{Name: "花花", Age: 13}
@@ -17,7 +20,21 @@ func main() {
 	ptr := &cat
 	cat.pointer = ptr
 	cat.friends = append(make([]Cat, 0), cat)
-	fmt.Printf("最终的猫是:%v", cat)
+	fmt.Printf("最终的猫是:%v\n", cat)
+
+	a := A{No: 1}
+	b := B{No: 2}
+	a = A(b)
+	a.test("test")
+	fmt.Printf("b对象强转为a:%v\n", a)
+
+	// 结构体序列化
+	bytes, _ := json.Marshal(cat)
+	jsonString := string(bytes)
+	fmt.Printf("cat序列化以后的字符串为:%v\n", jsonString)
+
+	person := Person{Name: "os7blue"}
+	person.goodMan()
 }
 
 func modify(cat Cat) {
@@ -28,9 +45,29 @@ func modify(cat Cat) {
 }
 
 type Cat struct {
-	Age     int
-	Name    string
+	Age     int    `json:"age"`
+	Name    string `json:"name"`
 	pointer *Cat
 	friends []Cat
 	info    map[string]string
+}
+
+type A struct {
+	No int
+}
+
+func (a A) test(name string) {
+	fmt.Printf("A对象的编号是:%v\n", a.No)
+}
+
+type B struct {
+	No int
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) goodMan() {
+	fmt.Printf("%s,你是一个好人", person.Name)
 }
